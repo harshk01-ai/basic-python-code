50f2c9c4fc71decea15c634027fb1c10ae30c377
harshk01-ai <h1653650@gmail.com>
2026-02-12 09:50:02 +0530

    new question

--- loops_basic_question.py
+++ loops_basic_question.py
@@ -1,3 +1,11 @@
+#1. Take a number n as input and print all numbers from 1 to n.
+n = int(input("Enter another number: "))
+i = 1
+
+while i <= n:
+    print(i)
+    i = i + 1
+
 #2. Take a number n as input and print the sum of all numbers from 1 to n.
 n = int(input("Enter  number: "))
 i = 1
@@ -6,3 +14,4 @@ while i <= n:
     sum = sum + i
     i = i + 1
 print(sum)
+
